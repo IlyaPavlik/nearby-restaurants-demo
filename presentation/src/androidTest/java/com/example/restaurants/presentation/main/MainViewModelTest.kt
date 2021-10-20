21358815d9c35e88aa09ee3ca3bc35685d9f0f4d
Ilya Pavlik <pavlik.1@mail.ru>
2021-10-20 20:09:08 +0700

    Started implementing of tests for view models

---
+++ presentation/src/androidTest/java/com/example/restaurants/presentation/main/MainViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.example.restaurants.presentation.main
+
+import org.junit.Test
+
+class MainViewModelTest {
+
+    @Test
+    fun mainViewModel_OpenMap() {
+        //TODO Check calling method of navigation manager
+    }
+
+    @Test
+    fun mainViewModel_OpenAppSettings() {
+        //TODO Check calling method of navigation manager
+    }
+
+}
---
+++ presentation/src/androidTest/java/com/example/restaurants/presentation/main/pages/map/MapViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.example.restaurants.presentation.main.pages.map
+
+import org.junit.Test
+
+class MapViewModelTest {
+
+    @Test
+    fun mapViewModel_onMapReady() {
+        //TODO Check subscribing to location updates and value in locationLiveData
+    }
+
+    @Test
+    fun mapViewModel_OnVisibleRegionChanged() {
+        //TODO Check getting venues from VenueInteractor
+    }
+
+}
