d27847c330ca5285d1f248a2cc62bc65ad6b7195
Ilya Pavlik <pavlik.1@mail.ru>
2021-10-21 08:24:20 +0700

    Started implementing of tests for data repositories

---
+++ data/src/androidTest/java/com/example/restaurants/data/repository/LocationRepositoryImplTest.kt
@@ -0,0 +1,12 @@
+package com.example.restaurants.data.repository
+
+import org.junit.Test
+
+class LocationRepositoryImplTest {
+
+    @Test
+    fun locationRepositoryImpl_ObserveCurrentLocation() {
+        //TODO Test observation of the current location using LocationServices
+    }
+
+}
---
+++ data/src/test/java/com/example/restaurants/data/repository/VenueRepositoryImplTest.kt
@@ -0,0 +1,22 @@
+package com.example.restaurants.data.repository
+
+import org.junit.Test
+
+class VenueRepositoryImplTest {
+
+    @Test
+    fun venueRepositoryImpl_FindRemoteVenues() {
+        //TODO Test search of venues via FoursquareService
+    }
+
+    @Test
+    fun venueRepositoryImpl_FindCachedVenues() {
+        //TODO Test search of venues in local cache
+    }
+
+    @Test
+    fun venueRepositoryImpl_SaveVenuesToCache() {
+        //TODO Test saving of venues to the cache
+    }
+
+}
